1e22d393dfcb87da420af8ed3805fb0f8e955801
Ptolemye <100210260+Ptolemye@users.noreply.github.com>
2022-06-16 15:12:24 +0800

    Add files via upload

---
+++ 垃圾回收和智能指针.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include<crtdbg.h>
+#include"stdlib.h"
+#include<string>
+using namespace std;
+template<class T>
+class smart_ptr
+{
+private:
+    bool own;
+    T* ptr;
+    
+public:
+    smart_ptr(T* _p = 0) :own(_p != 0), ptr(_p){};
+    ~smart_ptr()
+    {
+        if (own)delete ptr;
+    }
+    T& operator*()
+    {
+        return *ptr;
+    }
+};
+int main()
+{
+    int* p = new int(10);
+    smart_ptr<int>pa(p);
+    cout << *p;
+}
+
